d12e369eea4130f5ec5c0b10277132f41aba9a03
fredlesieur <fred.lesieur@hotmail.fr>
2024-09-04 11:00:53 +0200

    deuxieme partie

--- classes/CompteCourant.php
+++ classes/CompteCourant.php
@@ -4,5 +4,39 @@
  */
 class CompteCourant extends Compte
 {
+private $decouvert;
+/**
+ * constructeur de compte courant
+ * @param string $nom
+ * @param float $montant
+ * @param int $decouvert 
+ * @return void
+ * */
 
+public function __construct(string $nom, float $montant, int $decouvert)
+{
+    // on transfere les informations nécessaires au constructeurs de Compte
+
+    parent::__construct($nom, $montant);
+    $this->decouvert = $decouvert;
+}
+
+public function getDecouvert():int
+{
+    return $this->decouvert;
+}
+public function setDecouvert(int $montant):self
+{
+    if ($montant >= 0) {
+        $this->decouvert = $montant;
+    }
+    return $this;
+}
+public function retirer(float $montant)
+{
+    // on verifie si le decouvert pertmet le retrait
+    if($montant>0 && $this->solde - $montant >= -$this->decouvert){
+$this->solde-=$montant;
+    }
+}
 }
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-require_once "classes/Compte.php";
-require_once "classes/CompteCourant.php";
-require_once "classes/CompteEpargne.php";
+require_once 'classes/Compte.php';
+require_once 'classes/CompteCourant.php';
+require_once 'classes/CompteEpargne.php';
 
 // on instancie le compte
-$compte1 = new CompteCourant("fred", 100);
+$compte1 = new CompteCourant("benoit",500,1000);
+
 var_dump($compte1);
 
