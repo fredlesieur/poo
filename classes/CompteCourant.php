9c828358c46997694f082e5a941f3b8cd0d7a71e
El Capitan <144474965+xavierelcapitan@users.noreply.github.com>
2024-09-04 11:14:49 +0200

    1er commit X
    1er commit Xavier en créant une nouvelle branche

--- classes/CompteCourant.php
+++ classes/CompteCourant.php
@@ -4,5 +4,35 @@
  */
 class CompteCourant extends Compte
 {
+  private $decouvert;
 
+  public function __construct(string $nom, float $montant, int $decouvert)
+  {
+    parent::__construct($nom, $montant);
+
+    $this->decouvert = $decouvert;
+  }
+
+  public function getDecouvert():int 
+  {
+  return $this->decouvert;
 }
+  public function setDecouvert(int $montant):self
+{
+  if($montant >= 0){
+    $this->decouvert = $montant;
+  }
+  return $this;
+}
+
+  public function retirer(float $montant)
+  {
+    if($montant > 0 && $this->solde - $montant >= -$this->decouvert){
+      $this->solde -= $montant;
+    }else {
+      echo 'solde insuffisant';
+    }
+  }
+}
+
+
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once "classes/CompteCourant.php";
 require_once "classes/CompteEpargne.php";
 
 // on instancie le compte
-$compte1 = new CompteCourant("fred", 100);
+$compte1 = new CompteCourant("fred", 500, 200);
+$compte1->setDecouvert(200);
 var_dump($compte1);
 
